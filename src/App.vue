729b04ca632a6dfa851ce4d4913036aa1f0a5af0
TreleaniFederico <treleani.federico@gmail.com>
2022-09-22 18:20:31 +0200

    add content

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
     <HeaderComponent />
+    <ContentComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import ContentComponent from "./components/ContentComponent.vue";
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
+    ContentComponent,
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ContentComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div>--> Content goes here --</div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'ContentComponent',
+}
+</script>
+
+<style scoped lang="scss">
+@mixin max-width{
+    max-width: 1000px;
+    margin: 0 auto;
+}
+main{
+    background-color: rgb(21, 19, 19);
+    div{
+        @include max-width;
+        display: flex;
+        color: white;
+        align-items: center;
+        justify-content: center;
+        height: 200px;
+    }
+}
+</style>
