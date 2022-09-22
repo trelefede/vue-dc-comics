e77314b491415f9619f99fa1c7733e14fffd508b
TreleaniFederico <treleani.federico@gmail.com>
2022-09-22 17:49:30 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
+    <HeaderComponent />
   </div>
 </template>
 
 <script>
-
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderComponent,
   }
 }
 </script>
@@ -19,6 +20,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,58 @@
 <template>
   <header>
-    
+        <img src="../assets/dc-logo.png" alt="logo-small">
+        <nav>
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
   </header>
 </template>
 
 <script>
 export default {
-
+    name: 'HeaderComponent',
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@mixin max-width{
+    max-width: 1000px;
+    margin: 0 auto;
+}
 
+header{
+    @include max-width;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+img{
+    width: 50px;
+    height: 50px;
+    padding: 20px 0;
+}
+
+ul{
+    display: flex;
+    list-style-type: none;
+    a{
+        padding: 20px; 
+        text-decoration: none;
+        color: #2f4f4f;
+        font-weight: bold;
+        font-size: 0.5rem;
+    }
+    
+}
 </style>
